bfb1046f3d227074058eea0c26729962789051f8
guopeng1129972 <381694179@qq.com>
2020-08-11 22:02:24 +0800

    播放音乐

--- electron-quick-start/renderer/index.js
+++ electron-quick-start/renderer/index.js
@@ -1,21 +1,25 @@
 const { ipcRenderer } = require('electron')
 const { $ } = require('./helper')
 
+let musicAudio = new Audio()
+let allTracks
+let currentTrack
 $('add-music-button').addEventListener('click', () => {
   ipcRenderer.send('add-music-window')
 })
 
 const renderListHTML = (tracks) => {
   const tracksList = $('trackList')
+  console.log(tracks)
   const tracksListHtml = tracks.reduce((html, track) => {
     html += `<li class="row music-track list-group-item d-flex  justify-content-between align-items-center">
     <div class="col-10">
       <i class="fa fa-music mr-2 text-secondary"></i>
       <b>${track.fileName}</b>
     </div>
-    <div>
-      <i class="fa fa-play mr-2 col-1"></i>
-      <i class="fa fa-window-close col-1"></i>
+    <div class="control-element">
+      <i class="fa fa-play mr-2 col-1" data-id="${track.id}"></i>
+      <i class="fa fa-window-close col-1" data-id="${track.id}"></i>
     </div>
     </li>`
 
@@ -26,9 +30,20 @@ const renderListHTML = (tracks) => {
 }
 
 ipcRenderer.on('getTracks', (event, tracks) => {
-  console.log('receive tracks', tracks)
+  // console.log('receive tracks', tracks)
+  allTracks = tracks
   renderListHTML(tracks)
+})
 
-
-
+$('trackList').addEventListener('click', (event) => {
+  event.preventDefault()
+  let { dataset, classList } = event.target
+  const id = dataset && dataset.id
+  if (id && classList.contains('fa-play')) {
+    //播放音乐
+    currentTrack = allTracks.find(track => track.id === id)
+    musicAudio.src = currentTrack.path
+    musicAudio.play()
+    classList.replace('fa-play', 'fa-pause')
+  }
 })
